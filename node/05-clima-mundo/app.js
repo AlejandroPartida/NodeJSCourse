1c834001f5c64c35dcf1df34d2643a253fff1450
Alejandro Partida <alex.9939@hotmail.com>
2021-01-11 19:09:46 -0600

    added Weather API data

--- node/05-clima-mundo/app.js
+++ node/05-clima-mundo/app.js
@@ -1,9 +1,26 @@
+const axios = require('axios');
+
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
         desc: 'Dirección de la ciudad para obtener el clima',
         demand: true
     }
-}).argv
+}).argv;
 
-console.log(argv.direccion);
+const encodedURL = encodeURI(argv.direccion);
+console.log(encodedURL);
+
+const instance = axios.create({
+    baseURL: `http://api.weatherapi.com/v1/current.json?key=481a9bfa99c7429c9d8235207211101&q=${encodedURL}`
+});
+
+
+
+instance.get()
+    .then(resp => {
+        console.log(resp.data.location);
+    })
+    .catch(err => {
+        console.log('ERROR!!!!!', err);
+    });
