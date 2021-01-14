d12a31b35aea0d9009e570e45f86008cb19ed099
Alejandro Partida <alex.9939@hotmail.com>
2021-01-13 20:36:57 -0600

    Weather app now running

--- node/05-clima-mundo/app.js
+++ node/05-clima-mundo/app.js
@@ -1,3 +1,8 @@
+const lugar = require('./lugar/lugar');
+const clima = require('./clima/clima');
+
+
+
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -6,4 +11,20 @@ const argv = require('yargs').options({
     }
 }).argv;
 
-//argv.direccion
+
+const getInfo = async(direccion) => {
+
+    try {
+        const l = await lugar.getLugarLatLng(direccion);
+
+        const temp = await clima.getClima(l.lat, l.lng);
+
+        return `El clima de ${l.location} es de ${temp}.`;
+    } catch (e) {
+        return `No se pudo determinar el clima de ${direccion}`
+    }
+}
+
+getInfo(argv.direccion)
+    .then(console.log)
+    .catch(console.log);
--- node/05-clima-mundo/lugar/lugar.js
+++ node/05-clima-mundo/lugar/lugar.js
@@ -1,24 +1,42 @@
 const axios = require('axios');
 
-const getLugarLatLng = (direccion) => {
+const getLugarLatLng = async(direccion) => {
+
+    const encodedURL = encodeURI(direccion);
+
+    const instance = axios.create({
+        baseURL: 'http://api.weatherapi.com/v1/current.json',
+        params: {
+            key: "481a9bfa99c7429c9d8235207211101",
+            q: encodedURL
+        }
+    });
 
 
+
+    const resp = await instance.get();
+
+    if (resp.data.error) {
+
+        throw `No hay resultados para ${direccion}`;
+
+    }
+
+    const data = resp.data;
+    const location = resp.data.location.name;
+    const lat = data.location.lat;
+    const lng = data.location.lon;
+
+
+    return {
+        location,
+        lng,
+        lat
+    }
 
 }
 
-const encodedURL = encodeURI(argv.direccion);
-console.log(encodedURL);
 
-const instance = axios.create({
-    baseURL: `http://api.weatherapi.com/v1/current.json?key=481a9bfa99c7429c9d8235207211101&q=${encodedURL}`
-});
-
-
-
-instance.get()
-    .then(resp => {
-        console.log(resp.data.location);
-    })
-    .catch(err => {
-        console.log('ERROR!!!!!', err);
-    });
+module.exports = {
+    getLugarLatLng
+}
